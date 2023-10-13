6b093fc59c4a36921e728799068b09b1a6d93f7b
Kamil Chaj <kamilchaj@student.agh.edu.pl>
2023-10-13 19:18:41 +0200

    done

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ new_file_name = "new.html"
 input_text = "Hello there"
 HTML_PATH = "HTML/{}"
 IMG_PATH = 'img/{}'
-
+dimensions = (900, 500)
 
 line_count = 5
 lines = []
@@ -16,20 +16,19 @@ line_prompt_text = 'Input text for line number {}: '
 contact_prompt_text = 'Input contact information\n enter empty line to finish: '
 new_contacts = True
 
-# for line in range(line_count):
-#     lines.append(input(line_prompt_text.format(line + 1)))
+for line in range(line_count):
+    lines.append(input(line_prompt_text.format(line + 1)))
 
-# print(contact_prompt_text)
-# while new_contacts:
-#     new_contact = input()
-#     if new_contact == '':
-#         new_contacts = False
-#         contacts.append('')
-#     else:
-#         contacts.append(new_contact)
+print(contact_prompt_text)
+while new_contacts:
+    new_contact = input()
+    if new_contact == '':
+        new_contacts = False
+    else:
+        contacts.append(new_contact)
 
-lines = ['test1', 'test2', 'test3', 'test4', 'test5']
-contacts = ['email@net.com', '911', 'net.com']
+# lines = ['Nunu and Willump', 'the boy and his yeti', 'adventurer', 'The Freljord is a harsh and unforgiving place', '\"This way to... adventure!" (Willump noises)\"']
+# contacts = ['nunu@willump.lol', '9999AP', 'snowball.gg']
 print(lines, contacts)
         
 HTMLFile = open(HTML_PATH.format(file_name), "r", encoding="utf8") 
@@ -58,7 +57,13 @@ for contact in contacts:
 X = BeautifulSoup(contact_arrow, 'lxml')
 parent_element.append(X)
 
+main_div = S.find(id='main')
+zoom_text = 'zoom: {};'
+zoom = 2.6
+main_div['style'] = zoom_text.format(zoom)
+
 new_file = open(HTML_PATH.format(new_file_name), 'w')
 new_file.write(S.prettify())
 
-Html2Image(output_path='img').screenshot(html_file=HTML_PATH.format(new_file_name), save_as='out.jpg')
+
+Html2Image(output_path='img', size=dimensions).screenshot(html_file=HTML_PATH.format(new_file_name), save_as='out.png')
